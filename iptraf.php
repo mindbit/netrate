c560598f2e41b1f38088c088af6ae7fb82a7f92d
Radu Rendec <radu.rendec@cloudbit.ro>
2013-04-08 14:37:26 +0300

    Enhanced iptraf.php
    * custom update delay
    * display current time and update delay
    * display total rates
    
    Signed-off-by: Radu Rendec <radu.rendec@cloudbit.ro>

--- iptraf.php
+++ iptraf.php
@@ -1,12 +1,41 @@
-<?
+<?php
 // Usage: tcpdump -qnnttl [options] | php iptraf.php
+
+class Stats {
+	public $bDown, $bUp;
+	public $cDown, $cUp;
+
+	function __construct($bDown = 0, $bUp = 0, $pDown = 0, $pUp = 0) {
+		$this->bDown = $bDown;
+		$this->bUp = $bUp;
+		$this->pDown = $pDown;
+		$this->pUp = $pUp;
+	}
+
+	function add($stats) {
+		$this->bDown += $stats->bDown;
+		$this->bUp += $stats->bUp;
+		$this->pDown += $stats->pDown;
+		$this->pUp += $stats->pUp;
+	}
+
+	function out($str, $factor) {
+		printf("%16s %16.2f %16.2f %16.2f %16.2f\n",
+				$str,
+				$this->bDown / 1024 / $factor,
+				$this->pDown / 1000 / $factor,
+				$this->bUp / 1024 / $factor,
+				$this->pUp / 1000 / $factor);
+	}
+}
 
 class Parser {
 	protected $lastTime;
 	protected $stats = array();
+	protected $delay = 2;
 
 	function trafficCmp($a, $b) {
-		return $b["b_down"] - $a["b_down"];
+		return $b->bDown - $a->bDown;
 	}
 
 	function printStats() {
@@ -14,20 +43,21 @@ class Parser {
 			return;
 		uasort($this->stats["IP"], array($this, "trafficCmp"));
 		echo "\033[2J\033[1;1H";
+		echo "           Every " . $this->delay . " seconds. Current time: " .
+			date("r") . "\n\n";
 		printf("%16s %16s %16s %16s %16s\n\n",
 				"IP",
 				"RX kbytes/s",
 				"RX kpackets/s",
 				"TX kbytes/s",
 				"TX kpackets/s");
-		foreach ($this->stats["IP"] as $ip => $data) {
-			printf("%16s %16.2f %16.2f %16.2f %16.2f\n",
-					$ip,
-					$data["b_down"] / 1024,
-					$data["p_down"] / 1000,
-					$data["b_up"] / 1024,
-					$data["p_up"] / 1000);
+		$total = new Stats();
+		foreach ($this->stats["IP"] as $ip => $stats) {
+			$stats->out($ip, $this->delay);
+			$total->add($stats);
 		}
+		echo "\n";
+		$total->out("TOTAL", $this->delay);
 	}
 
 	function match($ip) {
@@ -37,10 +67,11 @@ class Parser {
 	function parse($line) {
 		list($time, $l3proto, $data) = explode(' ', $line, 3);
 		$time = explode('.', $time);
+		$curTime = (int)floor($time[0] / $this->delay);
 
-		if ($time[0] != $this->lastTime) {
+		if ($curTime != $this->lastTime) {
 			$this->printStats();
-			$this->lastTime = $time[0];
+			$this->lastTime = $curTime;
 			$this->stats = array();
 		}
 		if (!isset($this->stats[$l3proto]))
@@ -55,10 +86,10 @@ class Parser {
 			$ip[1] = implode('.', explode('.', $ip[1], -1));
 			if ($this->match($ip[0])) {
 				$ip = $ip[0];
-				$dir = "up";
+				$dir = "Up";
 			} elseif ($this->match($ip[1])) {
 				$ip = $ip[1];
-				$dir = "down";
+				$dir = "Down";
 			} else
 				break;
 
@@ -74,14 +105,12 @@ class Parser {
 			}
 
 			if (!isset($stats[$ip]))
-				$stats[$ip] = array(
-						"b_up" => 0,
-						"b_down" => 0,
-						"p_up" => 0,
-						"p_down" => 0
-						);
-			$stats[$ip]["b_" . $dir] += $len;
-			$stats[$ip]["p_" . $dir]++;
+				$stats[$ip] = new Stats();
+
+			$bm = "b" . $dir;
+			$pm = "p" . $dir;
+			$stats[$ip]->$bm += $len;
+			$stats[$ip]->$pm ++;
 			break;
 		default:
 			//echo "Ignoring unknown proto '" . $l3proto . "'\n";
